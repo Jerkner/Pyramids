45ab5b7785a0fcfd237dda44cec295a2f7a8d238
Jerkner <philip@jerkner.se>
2023-12-03 17:45:19 +0000

    Update index.js

--- index.js
+++ index.js
@@ -59,7 +59,9 @@ function renderDeckCard() {
             : "Back.png"
 
     const pText = cardsInDeck
-        ? `<p>Click to<br>draw a card<span class="remaining">${cardsInDeck} remaining</span></p>`
+        ? `<p>Click to<br>draw a card<span class="remaining">${
+              cardsInDeck - 1
+          } remaining</span></p>`
         : `<p>You are out of cards.<br><br>Click here to play again.</p>`
     const cardHtml = `<div class="card deck-card">
                         ${pText}

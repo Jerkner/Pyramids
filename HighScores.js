c47f1e97ad43798d0264e01554778725ca55ddff
Jerkner <philip@jerkner.se>
2024-01-04 15:52:28 +0100

    Update HighScores.js

--- HighScores.js
+++ HighScores.js
@@ -1,9 +1,14 @@
 const mongoose = require("mongoose")
 
-const highScoreSchema = new mongoose.Schema({
-    playerName: String,
-    score: Number,
-})
+const highScoreSchema = new mongoose.Schema(
+    {
+        playerName: String,
+        score: Number,
+    },
+    {
+        timestamps: true, // Add timestamps for createdAt and updatedAt
+    }
+)
 
 const HighScore = mongoose.model("HighScore", highScoreSchema)
 
